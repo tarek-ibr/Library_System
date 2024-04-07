d4f0c6845a67677dcd1833cedf274110c5fac013
LEVI <ahmedatef15011@gmail.com>
2024-04-08 00:29:07 +0200

    Edit book's data 2

--- Librarian.cpp
+++ Librarian.cpp
@@ -8,19 +8,27 @@ Librarian::Librarian(const Custom_String_Class& name, int ID) : Member(name, ID,
 void Librarian::addBook() {
     // add a book to the library inventory
     Custom_String_Class title;
+    cout << "Enter book's title: " << endl;
     cin >> title;
     Custom_String_Class isbn;
     cin >> isbn;
     Custom_String_Class author;
+    cout << "Enter author's name: " << endl;
     cin >> author;
     Custom_String_Class genre;
+    cout << "Enter book's genre: " << endl;
     cin >> genre;
     Custom_String_Class duedate;
+    cout << "Enter book's duedate: " << endl;
     cin >> duedate;
     int publicationYear;
+    cout << "Enter book's publication year: " << endl;
     cin >> publicationYear;
     bool availablity;
-    cin >> availablity;
+    cout << "Enter whether the book is available or not(Y/N): " << endl;
+    char ans;
+    cin >> ans;
+    (ans == 'Y')? availablity = 1: availablity = 0;
     Book newBook(title,author,isbn,genre,duedate,publicationYear,availablity);
     Book::Book_List.push_back(newBook);
 }
@@ -38,43 +46,43 @@ void Librarian::removeBook(const Custom_String_Class& ISBN) {
 void Librarian::editBook(Book& book)
 {
     char answer;
-    cout << "Do you want to edit the title ?(Y/N)";
+    cout << "Do you want to edit the title ?(Y/N)" << endl;
     cin >> answer;
     if(answer == 'Y')
     {
         cin >> book.Title;
     }
-    cout << "Do you want to edit the ISBN ?(Y/N)";
+    cout << "Do you want to edit the ISBN ?(Y/N)" << endl;
     cin >> answer;
     if(answer == 'Y')
     {
         cin >> book.ISBN;
     }
-    cout << "Do you want to edit the author ?(Y/N)";
+    cout << "Do you want to edit the author ?(Y/N)" << endl;
     cin >> answer;
     if(answer == 'Y')
     {
         cin >> book.Author;
     }
-    cout << "Do you want to edit the genre ?(Y/N)";
+    cout << "Do you want to edit the genre ?(Y/N)" << endl;
     cin >> answer;
     if(answer == 'Y')
     {
         cin >> book.Genre;
     }
-    cout << "Do you want to edit the due date ?(Y/N)";
+    cout << "Do you want to edit the due date ?(Y/N)" << endl;
     cin >> answer;
     if(answer == 'Y')
     {
         cin >> book.duedate;
     }
-    cout << "Do you want to edit the publication year ?(Y/N)";
+    cout << "Do you want to edit the publication year ?(Y/N)" << endl;
     cin >> answer;
     if(answer == 'Y')
     {
         cin >> book.Publication_Year;
     }
-    cout << "Do you want to edit the availability ?(Y/N)";
+    cout << "Do you want to edit the availability ?(Y/N)" << endl;
     cin >> answer;
     if(answer == 'Y')
     {

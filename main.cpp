21283f128d8893c3a38089f2a60b92a394b1ce38
Tarek Ibrahim <tarek.ibr007@gmail.com>
2024-04-21 19:11:26 +0200

    updates v2.1

--- main.cpp
+++ main.cpp
@@ -18,12 +18,16 @@ int main() {
     Librarian::loadLibrarian();
     Book::loadlibrary();
 
-    Member m1("Seif", 1234, "student");
-    Librarian l1("Ammar", 5678);
+    Member m1("tamer", 77777, "Student");
+    Librarian l1("elgayar", 88888);
 
     Book b1= Book::findByName("1984");
 
     m1.requestBorrow(b1);
+    l1.displayRequests();
+
+    //vector<Loan>& ln= Member::getCheckedOutBooks();
+    //l1.approveBorrowRequest();
 
 
 

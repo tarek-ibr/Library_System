b24ae372dd34ec2567370e2fc77646976d39b733
Youssef Ayman <62572632+ELJoOker2004@users.noreply.github.com>
2024-04-26 18:19:29 +0300

    Commenting Functionalities.h

--- Functionalities.h
+++ Functionalities.h
@@ -1,6 +1,4 @@
-//
 // Created by tarek on 4/22/2024.
-//
 
 #ifndef PROJECT_FUNCTIONALITIES_H
 #define PROJECT_FUNCTIONALITIES_H
@@ -9,28 +7,49 @@
 #include "User.h"
 #include <string>
 
-
+// Function to load all data from files
 void loadFiles();
+
+// Function to save all data to files
 void saveFiles();
 
-//Custom_String_Class *executeGUIOption(Custom_String_Class *options, size_t num_options);
-
+// Function to login a user
 Custom_String_Class login(int);
 
+// Function to load all data from files
 void LoadFiles();
+
+// Function to save all data to files
 void SaveFiles();
 
+// Function to display the menu for a member
 void diplayMenuMember();
+
+// Function to display the menu for a librarian
 void displayMenuLibrarian();
+
+// Function to display the menu for finding a book
 void displayMenuFindBook();
+
+// Function to display the menu for finding a member
 void displayMenuFindMember();
+
+// Function to display the menu for finding a librarian
 void displayMenuFindlibrarian();
 
+// Function to implement the choice of a member
 void implementMemberChoice(Member&, int);
-void implementLibrarianChoice(Librarian&, int);
-void implementFindBookChoice(int);           //all finds has logical errors does not cin
-void implementFindMemberChoice(int);
-void implementFindLibrarianChoice(int);
 
+// Function to implement the choice of a librarian
+void implementLibrarianChoice(Librarian&, int);
+
+// Function to implement the choice of finding a book
+void implementFindBookChoice(int);
+
+// Function to implement the choice of finding a member
+void implementFindMemberChoice(int);
+
+// Function to implement the choice of finding a librarian
+void implementFindLibrarianChoice(int);
 
 #endif //PROJECT_FUNCTIONALITIES_H
